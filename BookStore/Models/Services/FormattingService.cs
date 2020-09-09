c6c793231f74cd17ddd8103f85e4e32ee0c09e5a
Nikolai Chernoglazov <nikolai.i.chernoglazov@outlook.com>
2020-09-09 19:55:19 +0300

    Add services for downloading and displaying images and for displaying dates and prices

---
+++ BookStore/Models/Services/FormattingService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.Services
+{
+    public class FormattingService
+    {
+        public FormattingService()
+        {
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+        }
+
+        public string FormatDate(DateTime? date)
+        {
+            return date?.ToString("D") ?? "-";
+        }
+
+        public string FormatPrice(decimal price)
+        {
+            return (price > 0) ? price.ToString("c") : "Free";
+        }
+    }
+}
---
+++ BookStore/Models/Services/ImageService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.Services
+{
+    public class ImageService
+    {
+        /// <summary>
+        /// Gets the image as a byte array from the given <see cref="IFormFile"/> object.
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns>A task containing the image byte array
+        /// and the image type string.</returns>
+        public async Task<(byte[], string)> GetImageFromFormFile(IFormFile file)
+        {
+            using var stream = new System.IO.MemoryStream();
+            await file.CopyToAsync(stream);
+
+            return (stream.ToArray(), file.ContentType.Replace("image/", ""));
+        }
+
+        /// <summary>
+        /// Downloads the image as a byte array from the given URL.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns>A task containing the image byte array
+        /// and the image type string.</returns>
+        public async Task<(byte[], string)> GetImageFromUrl(string url)
+        {
+            using var httpClient = new System.Net.Http.HttpClient();
+
+            return (await httpClient.GetByteArrayAsync(url), "jpeg");
+        }
+
+        /// <summary>
+        /// Gets a Base64 data URI encoding of an image,
+        /// which can be embedded into HTML files
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="imageType"></param>
+        /// <returns>A Base64 image data URI string.</returns>
+
+        public string GetBase64Encoding(byte[] image, string imageType)
+        {
+            if (image == null || imageType == null)
+                return null;
+
+            var base64Image = Convert.ToBase64String(image);
+            return $"data:image/{imageType};base64,{base64Image}";
+        }
+    }
+}
