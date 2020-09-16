40e64bf943f3b95b5c51ebaa375bd45f369a5c21
Nikolai Chernoglazov <nikolai.i.chernoglazov@outlook.com>
2020-09-16 22:27:36 +0300

    Add PaginatedList class and sorting, filtering and pagination to the Authors Management page

--- BookStore/Areas/Admin/Pages/Database/Authors/Index.cshtml.cs
+++ BookStore/Areas/Admin/Pages/Database/Authors/Index.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookStore.Infrastructure;
 using BookStore.Models;
 using BookStore.Models.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -19,11 +20,58 @@ namespace BookStore.Areas.Admin.Pages.Database.Authors
             _repository = repository;
         }
 
-        public IList<Author> Authors { get; set; }
 
-        public async Task OnGetAsync()
+        public PaginatedList<Author> Authors { get; set; }
+
+        public string IdSort { get; set; }
+        public string NameSort { get; set; }
+        public string DateOfBirthSort { get; set; }
+        public string CurrentSort { get; set; }
+
+        public string CurrentFilter { get; set; }
+
+
+        public async Task OnGetAsync(string sortOrder,
+            string currentFilter, string searchString, int? pageIndex)
         {
-            Authors = await _repository.Authors.ToListAsync();
+            CurrentSort = sortOrder;
+            IdSort = sortOrder == "id" ? "id_desc" : "id";
+            NameSort = sortOrder == "name" ? "name_desc" : "name";
+            DateOfBirthSort = sortOrder == "dateOfBirth" ? "dateOfBirth_desc" : "dateOfBirth";
+
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            CurrentFilter = searchString;
+
+            var authorsQuery = from a in _repository.Authors
+                               select a;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                authorsQuery = authorsQuery.Where(
+                    a => a.Name.ToLower().Contains(
+                        searchString.ToLower()));
+            }
+
+            authorsQuery = sortOrder switch
+            {
+                "id_desc" => authorsQuery.OrderByDescending(a => a.AuthorId),
+                "name" => authorsQuery.OrderBy(a => a.Name),
+                "name_desc" => authorsQuery.OrderByDescending(a => a.Name),
+                "dateOfBirth" => authorsQuery.OrderBy(a => a.DateOfBirth),
+                "dateOfBirth_desc" => authorsQuery.OrderByDescending(a => a.DateOfBirth),
+                _ => authorsQuery.OrderBy(a => a.AuthorId),
+            };
+
+            int pageSize = 5;
+            Authors = await PaginatedList<Author>.CreateAsync(
+                authorsQuery.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
 
         public async Task<IActionResult> OnPostDelete(int? id)
